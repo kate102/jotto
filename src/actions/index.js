425ad7116d98b142de2bca7842cfbc3e323ad355
Kate Morris <kmmorris@deloitte.co.uk>
2019-09-20 16:14:27 +0100

    Added guessed word reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,13 @@
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD'
 }
 /**
  * 
  */
 
-export function correctGuess() {
-    return { type: actionTypes.CORRECT_GUESS };
+export const guessWord = (guessWord) => {
+    return function(dispatch, getState) {
+
+    }
 }
--- src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionTypes } from './';
-
-describe ('correctGuess', () => {
-    test('returns an action with a type `CORRECT_GUESS`', () => {
-        const action  = correctGuess();
-        expect(action).toEqual({ type: actionTypes.CORRECT_GUESS  });
-    });
-})
